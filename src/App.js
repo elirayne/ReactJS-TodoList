b3d17918d32369cacb258d178add99273963ce9a
Eli Boucher <elenaivette@gmail.com>
2018-03-06 20:57:04 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+import List from './List';
+import TodoForm from './TodoForm';
+import Footer from './Footer';
+
+class App extends Component {
+  state = { todos: [], filter: 'All' }
+
+  setFilter = (filter) => {
+    this.setState({ filter })
+  }
+
+  getUniqId = () => {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1)
+  }
+
+  addItem = (name) => {
+    const { todos } = this.state
+    const todo = { name, id: this.getUniqId(), complete: false }
+    this.setState({ todos: [todo, ...todos] })
+  }
+
+  handleClick = (id) => {
+    const { todos } = this.state;
+    this.setState({
+      todos: todos.map( todo => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            complete: !todo.complete
+          }
+        }
+        return todo
+      })
+    })
+  }
+
+  visibleItems = () => {
+    const { todos, filter } = this.state
+    switch(filter) {
+      case 'Active':
+        return todos.filter( t => !t.complete )
+      case 'Complete':
+        return todos.filter( t => t.complete )
+      default:
+        return todos;
+    }
+  }
+
+  render() {
+    const { todos, filter } = this.state;
+ 
+    return (
+      <div>
+        <TodoForm addItem={this.addItem} />
+        <List name="Todo List" items={this.visibleItems()} todoClick={this.handleClick} />
+        <Footer filter={filter} setFilter={this.setFilter} />
+      </div>
+    );
+  }
+}
+
+export default App;
